55a31c8f02d89ee525c95eb328d149bf9dcdccc6
Andrii Sachuk <ansachuk@icloud.com>
2026-09-07 13:51:38 +0200

    finished hw WorkWithBytes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
+import hw.io.WorkWithBytes;
+
 public class Main {
     public static void main(String[] args) {
+        WorkWithBytes bytes = new WorkWithBytes();
+
+        String[] arr = new String[]{"AAA", "bbb", "CcC"};
+
+        for (String el : arr) {
+            bytes.writeBytesToFile("src/test.txt", el.getBytes());
+        }
     }
 }
---
+++ src/main/java/hw/io/WorkWithBytes.java
@@ -0,0 +1,23 @@
+package hw.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+public class WorkWithBytes {
+    //Тепер потренуймося записувати масив байтів у файл та реалізуємо метод writeBytesToFile(),
+    // який приймає 2 параметри:
+    //String fileName — це назва файлу, куди потрібно записати масив байтів;
+    //byte[] data — це дані, які потрібно записати.
+    //Зверни увагу: не перетворюй байти на String. У файл потрібно записати саме byte[].
+    public void writeBytesToFile(String fileName, byte[] data) {
+        File file = new File(fileName);
+
+        try {
+            Files.write(file.toPath(), data, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file " + fileName, e);
+        }
+    }
+}
